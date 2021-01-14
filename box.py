fc654a6561c8b496d6f85ecb806631ff9896c94e
41AnkitBisht <41ankitbisht@gmail.com>
2021-01-14 09:52:54 +0530

    Beverage Problem v1.1

--- box.py
+++ box.py
@@ -1,9 +1,10 @@
 import random
 
 class temprange:
-    def __init__(self, high, low):
+    def __init__(self, high, low, boxname):
         self.high = high
         self.low=low
+        self.boxname = boxname
         self.nextval = None
 
 class randomtemp:
@@ -15,8 +16,8 @@ class LinkedList:
     def __init__(self):
         self.headval = None
 
-    def insertcontainer(self, hightemp, lowtemp):
-        NewBox = temprange(hightemp, lowtemp)
+    def insertcontainer(self, hightemp, lowtemp, box_name):
+        NewBox = temprange(hightemp, lowtemp, box_name)
         if self.headval is None:
             self.headval = NewBox
             return
@@ -33,19 +34,20 @@ class LinkedList:
         while checkval:
             print("Current temp:" ,newrandomval.randomval,  "Temperature Range:"  ,checkval.low,  "-"  ,checkval.high)
             if newrandomval.randomval < checkval.low:
-                print("Message alert: warninglow")
+                print("Message alert:", checkval.boxname,"Low Temperature")
             elif newrandomval.randomval > checkval.high:
-                print("Message alert :warninghigh")
+                print("Message alert :", checkval.boxname, "High Temperature ")
             else:
                 print("All ok")
             newrandomval = randomtemp()
             checkval = checkval.nextval
 
 Box = LinkedList()
-Box.insertcontainer(5,3)
-Box.insertcontainer(6,4)
-Box.insertcontainer(7,3)
-Box.insertcontainer(8,2)
+Box.insertcontainer(5,3, "Box1")
+Box.insertcontainer(6,4,"Box2")
+Box.insertcontainer(7,3, "Box3")
+Box.insertcontainer(8,2, "Box4")
+Box.insertcontainer(7,5, "Box5")
 
 
 Box.checkbox()
